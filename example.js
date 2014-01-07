f883cf01568fc4f70f79a5747310bcc2bdd2f249
JayChetty <jaypaulchetty@gmail.com>
2014-01-07 19:46:35 +0000

    fix

--- example.js
+++ example.js
@@ -1,6 +1,14 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '192.168.1.100');
-console.log('Server running at http://192.168.1.100:1337/');
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
+
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
